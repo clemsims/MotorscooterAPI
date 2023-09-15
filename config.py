f2fd312b25dac9197651571d36b8b6758ce3d776
Clément Simon <55788593+clemsims@users.noreply.github.com>
2023-09-15 11:07:22 +0200

    Update config.py
    Removed API (deprecated anyways) secret keys from project

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 global token 
-token = str('827f0c5b-97a0-4676-8b35-c77670e530e6') # token for the API of roote.io
+token = str('ENTER YOUR API KEY HERE') # token for the API of roote.io
 
 global api_key 
-api_key = str('rHzJqM4syWmOIHXteFOxhWSLCMYDL7Dp250fs0fP5RQ') # api key for the API of HERE maps
+api_key = str('ENTER YOUR API KEY HERE') # api key for the API of HERE maps
